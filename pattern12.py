1fbcb2c5c90cbb2a8f6b729c480e699233aa5639
sivasankars-dev <sivasankars.dev@gmail.com>
2025-06-17 06:09:34 +0530

    TODAY Practice

--- pattern12.py
+++ pattern12.py
@@ -1,1 +1,20 @@
+# Can you print below pattern
+# 1             1 
+# 1 2         2 1 
+# 1 2 3     3 2 1 
+# 1 2 3 4 4 3 2 1 
 
+n = 4
+for i in range(n):
+    for j in range(n):
+        if i >= j:
+            print(j+1, end=" ")
+        else:
+            print(" ", end=" ")
+
+    for k in range(n):
+        if n-1-i <= k:
+            print(n-1-k+1, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
